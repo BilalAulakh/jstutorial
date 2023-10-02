aab366edbc7953eed1c7c02cb40de074609b4cb1
BilalAulakh <146110318+BilalAulakh@users.noreply.github.com>
2023-10-02 12:28:05 +0000

    strings

---
+++ jsproj/string.js
@@ -0,0 +1,45 @@
+// const nameis="Bilal";
+// const age=27;
+// console.log(`my name is ${nameis} and age is ${age}`)
+
+
+
+// console.log(myname);
+// console.log(myname[0]);
+
+
+// console.log(myname.length);
+// console.log(myname.toUpperCase());
+
+// console.log(myname.charAt(3))
+// console.log(myname.indexOf('l'))
+
+// const newstring=myname.substring(2,6);
+// console.log(newstring);
+
+// const newstring2=myname.slice(2,7);
+// console.log(newstring2);
+
+// const strings='   Bilal  '
+// console.log(strings)
+// console.log(strings.trim())
+
+// const urls="http://bilal.com/jutt%20countory";
+
+// console.log(urls.replace('%20', '-'))
+
+// const myname=new String("Bilal-jutt")
+// console.log(myname.split('-'))
+
+// const balance=new Number(100);
+// console.log(balance);
+// console.log(balance.toString().length)
+// console.log(typeof balance);
+// console.log(balance.toFixed(1))
+
+
+// const othernumber=34.3848;
+// console.log(othernumber.toPrecision(1))
+
+const hundreds=100000;
+console.log(hundreds.toLocaleString('en-IN'))
