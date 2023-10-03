3d91ad45496378f4c0c081277337e7d62448e525
BilalAulakh <146110318+BilalAulakh@users.noreply.github.com>
2023-10-03 18:54:39 +0000

    sliceandsplice

---
+++ jsproj/Array.js
@@ -0,0 +1,20 @@
+const aray=[8,4,3,4,2,9];
+// console.log()
+
+// const newAray=aara.join()
+
+// console.log(aara)
+// console.log(typeof newAray)
+
+console.log("A",aray);
+const myn1=aray.slice(1,3);
+console.log(myn1);
+console.log("B",aray);
+
+const myn2=aray.splice(1,3);
+console.log(myn2)
+console.log("C",aray)
+
+
+
+// slice splice
