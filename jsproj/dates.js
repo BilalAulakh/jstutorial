421b7bf29a77dd9d7acdab9345185a52d4ef216e
BilalAulakh <146110318+BilalAulakh@users.noreply.github.com>
2023-10-03 18:11:49 +0000

    array

---
+++ jsproj/dates.js
@@ -0,0 +1,25 @@
+let mydate =new Date();
+
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString())
+
+// console.log(typeof mydate)
+
+// let mycreateddate=new Date(2023,0,23);
+
+// let mycreateddate=new Date(2023,0,23)
+let mycreateddate=new Date("01-12-2023");
+// console.log(mycreateddate.toLocaleString());
+
+
+let myTimestamp=Date.now();
+
+// console.log(myTimestamp);
+// console.log(mycreateddate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
+
+
+
+
