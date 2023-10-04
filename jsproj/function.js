1578855124130208fdf6325931877836aab9f695
BilalAulakh <146110318+BilalAulakh@users.noreply.github.com>
2023-10-04 18:10:41 +0000

    functions

---
+++ jsproj/function.js
@@ -0,0 +1,30 @@
+// function addtwo(num1,num2){
+//     // console.log(num1+num2);
+// return num1+num2;
+// }
+// const result=addtwo(2,5);
+// console.log(result);
+
+// function calculateprice(...num1){
+// return num1;
+// }
+
+// console.log(calculateprice(20,39,48));
+
+// const user = {
+//     username: "Bilal",
+//     price: 666
+// }
+
+// function handleObject(anyobject) {
+//     console.log(`user name is ${anyobject.username} the price is ${anyobject.price}`)
+// }
+
+// handleObject(user)
+
+const mynewarray=[20,50,66,33,[55,88,99],[44,77,99]]
+
+function arrayfun(getarry){
+    return getarry
+}
+console.log(arrayfun(mynewarray.flat(Infinity)));
