4ddef12c21568b18e9b582f24d7d80653ae09e51
BilalAulakh <146110318+BilalAulakh@users.noreply.github.com>
2023-10-04 11:01:33 +0000

    object1

---
+++ jsproj/objects.js
@@ -0,0 +1,26 @@
+// Object literals
+const mysym=Symbol('key1')
+const object={
+    name:"Bilal",
+    [mysym]:'mykey1',
+    age:25,
+    email:"bilaljut@gmail.com",
+    isLogged:false,
+    lastLogindays:['Monday','Sunday']
+
+}
+console.log(object['age'])
+
+console.log(typeof object[mysym]);
+console.log(object)
+
+object.greeting=function(){
+    console.log('this is function')
+}
+
+console.log(object.greeting());
+
+object.greeting2=function(){
+    console.log(`this is function  ${this.name}`)
+}
+console.log(object.greeting2())
