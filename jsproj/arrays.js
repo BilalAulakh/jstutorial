9bd39c027ae4c98e01c51ce91fbb5dcb2418f0a9
BilalAulakh <146110318+BilalAulakh@users.noreply.github.com>
2023-10-04 10:04:09 +0000

    arrays

---
+++ jsproj/arrays.js
@@ -0,0 +1,29 @@
+const array1=['thor','batman','spiderman']
+const dc_heros=['supermain','ironman','flash']
+
+// array1.push(dc_heros);
+// console.log(array1);
+
+// const newarray=array1.concat(dc_heros ) return new array (concat)
+
+// console.log(newarray);
+// console.log(typeof newarray);
+
+//  const newarray=[...array1,...dc_heros]
+//  console.log(newarray);
+
+//  console.log(typeof newarray)
+
+// const another=[1,3,4,[5,6,7],8,[5,8,[5,99]]]
+
+// const another_real=another.flat(1)
+// console.log(another_real);
+
+// console.log(Array.isArray('Bilaljutt'))
+// console.log(Array.from('Bilaljutt'))
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3))
