194c04bc8b92624425dd1cd4e1835b424f1dd833
BilalAulakh <146110318+BilalAulakh@users.noreply.github.com>
2023-10-05 12:47:45 +0000

    loops

---
+++ jsproj/loops.js
@@ -0,0 +1,20 @@
+for (let i = 1; i <= 10; i++) {
+    // console.log(i)
+    for (let j = 1; j <= 10; j++) {
+    //    console.log(i+'*'+j+'='+i*j)
+        
+    }
+    
+}
+
+//break and continue
+
+for (let i = 1; i <=20; i++) {
+
+    if(i==5){
+        console.log(`detected 5`)
+        break
+    }
+    console.log(`value of i is ${i}`)
+    
+}
