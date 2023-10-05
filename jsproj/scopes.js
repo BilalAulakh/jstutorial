cb7579a748af78219c681bee93709158acd16a34
BilalAulakh <146110318+BilalAulakh@users.noreply.github.com>
2023-10-05 05:13:17 +0000

    scopeofjs

---
+++ jsproj/scopes.js
@@ -0,0 +1,11 @@
+function one(){
+    const username='Bilal'
+
+    function two(){
+        const secondna='jutt'
+        console.log(username)
+    }
+    // console.log(secondna);
+    two() 
+}
+one()
