2b195b5767a3e66861d22fa0014eedda6739e7b1
BilalAulakh <146110318+BilalAulakh@users.noreply.github.com>
2023-10-05 10:21:42 +0000

    one

---
+++ jsproj/controle_flow/one.js
@@ -0,0 +1,17 @@
+// const balance =1000
+
+// if(balance<500){
+//     console.log('Less than 500')
+// }else if(balance<750){
+//     console.log('Less than 750')
+// }
+// else{
+//     console.log('Less than 1200')
+// }
+
+const userLoggedin=true
+const debitcard=true
+
+if(userLoggedin && debitcard){
+    console.log('purchase something')
+}
