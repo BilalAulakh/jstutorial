b478c36046eeccfec478e9bba886c77803fc5a98
BilalAulakh <146110318+BilalAulakh@users.noreply.github.com>
2023-10-05 07:11:54 +0000

    arrowfunction

---
+++ jsproj/arrowfunction.js
@@ -0,0 +1,28 @@
+// const user={
+//     username:"Bilal",
+//     price:33,
+//     welcomemessage:function(){
+//         console.log(this.username)
+//         // console.log(this);
+//     }
+// }
+
+// user.welcomemessage();  
+// user.username='jutt'
+// user.welcomemessage()
+ //this keword current context ko reffer krta ha
+//this ki evalue node ky ander {} empty object ati ha aur browser py window ati ha
+//  console.log(this)
+
+// function chai(){
+//     let username='bilal'
+//     console.log(this.username);
+// }
+// chai()
+
+//this keword sirf ap object ky ander hi kam krta ha function ky ander ni kam krta 
+
+
+const addtwo=()=>{
+    
+}
