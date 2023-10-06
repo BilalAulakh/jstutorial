8aae41241accd9b808cfbb135e39bb854ad7d62d
BilalAulakh <146110318+BilalAulakh@users.noreply.github.com>
2023-10-06 05:41:12 +0000

    loops2

---
+++ jsproj/loops2.js
@@ -0,0 +1,19 @@
+// let index=0
+
+// while (index<10) {
+//     console.log(`value of index ${index}`)
+//     index=index+2
+// }
+
+// const arrays=['bilal','nasir','amir']
+// let arr=0
+// while (arr<arrays.length) {
+//     console.log(`this is array ${arrays[arr]}`)
+//     arr=arr+1
+// }
+let score=21
+do {
+    console.log(`score is ${score}`)
+    score++
+
+} while (score<=20);
