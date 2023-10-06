36417c19d83332eac57bc3b20e7eee054bbd61e5
BilalAulakh <146110318+BilalAulakh@users.noreply.github.com>
2023-10-06 06:21:26 +0000

    objectiterable

---
+++ jsproj/objectiterable.js
@@ -0,0 +1,10 @@
+const objects={
+    js:"javascript",
+    cpp:'C++',
+    swift:'swift by apple',
+    rb:'ruby'
+}
+
+for (const key in objects) {
+   console.log(`${key} shortcut of ${objects[key]}`)
+}
